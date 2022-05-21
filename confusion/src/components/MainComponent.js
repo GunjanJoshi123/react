d59e781e18264c00f4d7aa54f2dacf978ccec6a4
Gunjan Joshi <gunjanjoshi052@gmail.com>
2022-05-21 05:10:31 +0000

    aboutus component

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -9,7 +10,6 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
-// import Home from './HomeComponent';
 import {Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -51,6 +51,7 @@ class Main extends Component {
               <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId}/>
               <Route exact path="/contactus" component={Contact} />
+              <Route exact path="/aboutus" component={() => <About leaders ={this.state.leaders} />} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
